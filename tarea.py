6b5aa9e71a0c3239bedb879ec6ae6da289b699e7
thomisanti <thomassantiago733@gmail.com>
2025-02-17 08:40:26 -0500

    Create tarea.py
    tarea

---
+++ tarea.py
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	double t1,t2,t3,promedio;
+	
+	cin>> t1;
+	cin>> t2;
+	cin>> t3;
+	
+	promedio = (t1+t2+t3)/3;
+	
+	cout<< promedio << endl;
+	
+	return 0;
+}
